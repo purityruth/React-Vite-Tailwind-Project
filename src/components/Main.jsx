ccad9f30285d45d8af2877358c9af56016ca21da
unknown <purity.ruth@tunycemedia.com>
2024-01-11 11:47:10 +0300

    Profile updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
-import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
+import profileImg from '../assets/g4.jpg'
+import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub} from 'react-icons/fa'
 
 const Main = () => {
   return (
     <div id='main'>
-        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="./bg1.jpg" alt="" />
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={profileImg} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
           <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center'>
             <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Purity Ruth</h1>
@@ -29,10 +30,21 @@ const Main = () => {
             />
             </h2>
             <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+            <a href="https://github.com/purityruth" target="_blank" rel="noopener noreferrer">
+              <FaGithub className='cursor-pointer' size={20}/> 
+            </a>
+            <a href="https://github.com/purityruth" target="_blank" rel="noopener noreferrer">
               <FaTwitter className='cursor-pointer' size={20}/>
-              <FaFacebookF className='cursor-pointer' size={20}/>
+            </a>
+            <a href="https://github.com/purityruth" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF className='cursor-pointer' size={20} />
+            </a>
+            <a href="https://github.com/purityruth" target="_blank" rel="noopener noreferrer">
               <FaInstagram className='cursor-pointer' size={20}/>
+            </a>
+            <a href="https://www.linkedin.com/in/purity-ruth-3940b9227/" target="_blank" rel="noopener noreferrer">
               <FaLinkedinIn className='cursor-pointer' size={20}/>
+            </a>
             </div>
           </div>
           </div>
